d294c6c93bdf85ff7f6adf54a11c374d1820635e
Carl Sutherland <carl@humblesoftware.com>
2015-08-14 13:54:07 -0700

    Initial commit.
    * a basic sample application for Acuity in php
    * use the Acuity SDK with composer
    * connect to the API using OAuth2.

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+require_once('./vendor/autoload.php');
+require_once('./config.php');
+
+ini_set('session.save_path', '');
+session_start();
+
+// Acuity OAuth Connection
+$acuity = new AcuitySchedulingOAuth([
+  'accessToken' => $_SESSION['acuityAccessToken'],
+  'clientId' => CLIENT_ID,
+  'clientSecret' => CLIENT_SECRET,
+  'redirectUri' => REDIRECT_URI
+]);
+
+// Router:
+$method = $_SERVER['REQUEST_METHOD'];
+$path   = $_SERVER['PATH_INFO'] ? $_SERVER['PATH_INFO'] : '/';
+
+if ($method === 'GET' && $path === '/') {
+
+  if ($acuity->isConnected()) {
+    $response = $acuity->request('me');
+    $me = $response['body'];
+    $response = $acuity->request('appointments', [
+      'minDate' => strftime('%Y-%m-%d'),
+      'maxDate' => strftime('%Y-%m-%d')
+    ]);
+  } else {
+    $authorizationUrl = $acuity->getAuthorizeUrl(['scope' => 'api-v1']);
+  }
+
+  include 'templates/index.tpl.php';
+
+} else
+if ($method === 'GET' && $path === '/oauth2') {
+  $response = $acuity->requestAccessToken($_GET['code']);
+  $accessToken = $response['body']['access_token'];
+  if ($accessToken) {
+    $_SESSION['acuityAccessToken'] = $accessToken;
+    header('Location: /');
+  }
+} else
+if ($method === 'GET' && $path === '/disconnect') {
+  session_destroy();
+  header('Location: /');
+} else {
+  // 404
+}
---
+++ templates/index.tpl.php
@@ -0,0 +1,16 @@
+<h1>Acuity Sample PHP Application.</h1>
+
+<p>
+This is the Acuity Sample PHP Application.
+<?php if (!$acuity->isConnected()) { ?>Click connect to get started!<?php } ?>
+</p>
+
+<?php if (!$acuity->isConnected()) { ?>
+<a href="<?php echo $authorizationUrl; ?>">Connect to Acuity</a>
+<?php } else { ?>
+<a href="/disconnect">Disconnect</a>
+<h2>Hello, <?php echo $me['name']; ?></h2>
+<p>Here is your client scheduling page:</p>
+<iframe src="<?php echo $me['schedulingPage']; ?>" width="800" height="800" frameBorder="0"></iframe>
+<script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script>
+<?php } ?>
