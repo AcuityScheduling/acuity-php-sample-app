a9c99feabd9a059b033d635893610106fc55c3bb
Carl Sutherland <carl@humblesoftware.com>
2015-08-19 08:46:40 -0700

    Add some example query parameters.

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -11,13 +11,27 @@ This is the Acuity Sample PHP Application.
   <a href="/disconnect">Disconnect</a>
   <h2>Hello, <?php echo $me['name']; ?></h2>
   <h2>Daily Agenda for <?php echo $today; ?>:</h2>
+  <?php if ($agenda) { ?>
   <ul>
-  <?php foreach ($agenda as $appointment) { ?>
+    <?php foreach ($agenda as $appointment) { ?>
     <li>
     <?php echo "{$appointment['time']} &ndash; {$appointment['endTime']}: {$appointment['firstName']} {$appointment['lastName']} with {$appointment['calendar']}"; ?>
     </li>
-  <?php } ?>
+    <?php } ?>
   </ul>
+  <?php } else { ?>
+  <p>No appointments today.</p>
+  <?php } ?>
   <h2>Client Scheduling Page:</h2>
-  <?php echo $acuity->getEmbedCode($me['id']); ?>
+  <div style="text-align: center;">
+  <?php
+  echo $acuity->getEmbedCode($me['id'], [
+    'width' => 800,
+    'query' => [
+      'first_name' => 'Bob',
+      'last_name' => 'Burger'
+    ]
+  ]);
+  ?>
+  </div>
 <?php } ?>
